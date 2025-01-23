baca58fd220f3e2885e8e6ae5181f888e2690bcf
janitha-visna <visnajanitha@gmail.com>
2025-01-23 23:02:01 +0530

    styling css

--- src/main/java/org/example/javafx_gui/HelloApplication.java
+++ src/main/java/org/example/javafx_gui/HelloApplication.java
@@ -13,7 +13,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage primarystage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
-        primarystage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/org/example/javafx_gui/styling.css").toExternalForm()); // Add CSS
+
+        primarystage.setScene(scene);
         primarystage.initStyle(StageStyle.UNDECORATED);
         primarystage.show();
     }
